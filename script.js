dc677043872133de4c469d75936e581caa6e2c32
harshkalani <kalaniharsh10@gmail.com>
2025-01-30 20:34:48 +0530

    my portfolio

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function fun(){
   alert("Download succesfully")
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let observer = new IntersectionObserver(
+      (entries) => {
+          entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                  entry.target.style.animation = "appear 1s ease-in-out forwards";
+              }
+          });
+      },
+      { threshold: 0.5 } // Trigger when 50% of the element is visible
+  );
+
+  document.querySelectorAll("#block").forEach((element) => {
+      observer.observe(element);
+  });
+});
